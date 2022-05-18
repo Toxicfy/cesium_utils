d6185a683fe9cf054b9f571e1d3377060700c907
Toxicfy <15927630261@163.com>
2022-05-18 23:31:35 +0800

    feat: 补充方法及注解

--- src/layer/BaseLayer.ts
+++ src/layer/BaseLayer.ts
@@ -1,9 +1,10 @@
+import * as Cesium from 'cesium'
 /**
  * 数据图层基类
  */
 class BaseLayer {
   protected _layerType: string
-  public _source: any // 确定完所有类型的 source 后写成联合类型
+  public _source: Cesium.Cesium3DTileset|Cesium.ImageryProvider // 确定完所有类型的 source 后写成联合类型
 
   constructor (layerType: string) {
     this._layerType = layerType
--- src/layer/TilesetLayer.ts
+++ src/layer/TilesetLayer.ts
@@ -7,10 +7,23 @@ import layerType from './layerType'
  */
 class TilesetLayer extends BaseLayer {
   public _source: Cesium.Cesium3DTileset
+  private _style: Cesium.Cesium3DTileStyle
 
   constructor (url: string, options = {}) {
     super(layerType.TILESET_LAYER)
     this._source = new Cesium.Cesium3DTileset(Object.assign({ url, options }))
+  }
+
+  public get style () : Cesium.Cesium3DTileStyle {
+    return this._style
+  }
+
+  // 设置 3DTile Style
+  setStyle (style: Cesium.Cesium3DTileStyle) {
+    if (style) {
+      this._style = style
+      this._source.style = style
+    }
   }
 }
 
