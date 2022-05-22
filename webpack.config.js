3824f46afb4cfbf142617b18abe9ac3fe2641767
Toxicfy <15927630261@163.com>
2022-05-23 01:41:39 +0800

    feat：添加css文件及配置支持，示例添加toolTip

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }
     ]
   },
   plugins: [
